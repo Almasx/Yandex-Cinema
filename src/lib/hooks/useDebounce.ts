e3ca89af2408cef845063353c5ae5e769549b820
Almasx <saparovalmas0@gmail.com>
2023-06-26 10:04:39 +0500

    deploy

--- src/lib/hooks/useDebounce.ts
+++ src/lib/hooks/useDebounce.ts
@@ -2,6 +2,7 @@ import _ from "lodash";
 import { useCallback } from "react";
 
 export default function useDebounce(delay = 750) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   return useCallback(
     _.debounce((setQuery: () => void) => setQuery(), delay),
     []
